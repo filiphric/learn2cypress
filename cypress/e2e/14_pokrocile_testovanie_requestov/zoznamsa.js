82eb4e862119cadc2dc7d411b678db3b73c472b8
lukasdura <xswatx@gmail.com>
2022-10-19 14:54:38 +0200

    created tests

--- cypress/e2e/14_pokrocile_testovanie_requestov/zoznamsa.js
+++ cypress/e2e/14_pokrocile_testovanie_requestov/zoznamsa.js
@@ -1,90 +1,47 @@
 /// <reference types="cypress" />
 
-describe('testovanie casu pomocou momentjs', () => {
-
-  beforeEach( () => {
-    
-    cy
-      .server()
-
-    cy
-      .route('POST', '/api/tasks')
-      .as('createTask')
-
-    cy
-      .request('DELETE', '/api/tasks')
-
-    cy
-      .visit('/board/')
-
-  })
-
-  it('vytvoreny task ma deadline o 3 dni', () => {
-
-    cy
-      .contains('Add a card...')
-      .click()
-
-    cy
-      .get('.List .TextArea')
-      .type('chlieb{enter}')
-
-    cy
-      .wait('@createTask')
-      .then( task => {
-
-
-      })
-
-  })
-  
-})
-
 const tasks = ['chlieb', 'mlieko', 'pivo', 'syr']
 
-describe('testovanie taskov pomocou lodash', () => {
+
 
   beforeEach( () => {
 
-    cy
-      .server()
+    cy.server()
+    cy.route('/api/boards/*')
+    .as('boarddetails')
 
-    cy
-      .route('/api/boards/*')
-      .as('boardDetails')
+    cy.request('DELETE', '/api/tasks')
+
     
-    cy
-      .request('DELETE', '/api/tasks')
+    tasks.forEach(task=>{
+
+
+
+
     
-    tasks.forEach( task => {
+    
+    cy.request({
+      method:'POST',
+      url:'/api/tasks',
+      body:{
+        title:task,
+        boardId:45051089223,
+        listId:63727221515
 
-      cy
-        .request('POST', '/api/tasks', { 
-          title: task, 
-          listId: null,
-          boardId: null
-        })
-
-    })
-
-    // ⚠️ dopln id boardu
-    cy
-      .visit('/board/')
-
+ }})
+     
   })
+})
 
   it('nazov druheho tasku v liste', () => {
 
-    cy
-      .wait('@boardDetails')
-      .then( board => {
+cy.visit('/board/45051089223')
+cy.wait('@boarddetails').then(task=>{
+let Taskindex = Cypress._.findIndex(task.response.body.tasks,{title:'pivo'})
+expect(Taskindex).to.eq(2)
 
-        let taskIndex = ''
 
-        expect(taskIndex).to.eq(1)
 
-      })
+})
 
   })
-  
-})
